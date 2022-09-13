919ccbdf3de1d6ce2a6c8c68e35bcfe68ddbc39c
Gwendl <gwendal.lasson@gmail.com>
2022-09-13 17:46:00 +0200

    fix handle craft

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,29 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
 import {
   OnCraft,
   OnRecipeAdd,
   OnRecipeDelete,
   OwnershipTransferred
-} from "../generated/Crafting/Crafting"
-import { Recipe, Craft } from "../generated/schema"
+} from "../generated/Crafting/Crafting";
+import { concat } from "@graphprotocol/graph-ts/helper-functions";
+import { Recipe, Craft } from "../generated/schema";
 
 export function handleOnCraft(event: OnCraft): void {
-  var craft = Craft.load(event.transaction.from.toHex())
+  var craftId = getCraftId(event.params.user, event.params.recipeId);
+  var craft = Craft.load(craftId);
   if (!craft) {
-    craft = new Craft(event.transaction.from.toHex())
-    craft.count = BigInt.fromI32(0)
+    craft = new Craft(craftId);
+    craft.count = BigInt.fromI32(0);
+    craft.user = event.params.user;
   }
   craft.count = craft.count.plus(BigInt.fromI32(1));
-  craft.user = event.params.user;
+
   craft.recipe = event.params.recipeId.toHex();
   craft.save()
+}
+
+export function getCraftId(user: Address, recipeId: BigInt): string {
+  return concat(user, Bytes.fromBigInt(recipeId)).toHex();
 }
 
 export function handleOnRecipeAdd(event: OnRecipeAdd): void {
